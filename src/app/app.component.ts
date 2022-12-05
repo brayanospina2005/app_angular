b57f82a232d0dd34ee1807c6d122b9c370678209
brayanospina2005 <ospinabrayan431@gmail.com>
2022-12-05 16:30:14 -0500

    first header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'brayanangular';
+  title = 'Brayan Ospina';
+  getName():void{
+    console.log('Hello Brayan')
+  }
 }
